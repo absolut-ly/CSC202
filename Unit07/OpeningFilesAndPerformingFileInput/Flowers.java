6df92cb58d19dfb54775dcdc78e0acfbee4a74f9
absolut-ly <44728387+absolut-ly@users.noreply.github.com>
2021-02-25 18:04:00 -0500

    Unit07 files
    Final versions for Unit 7 from Cengage

---
+++ Unit07/OpeningFilesAndPerformingFileInput/Flowers.java
@@ -0,0 +1,22 @@
+
+
+// Flowers.java - This program reads names of flowers and whether they are grown in shade or sun from an input 
+// file and prints the information to the user's screen. 
+// Input:  flowers.dat.
+// Output: Names of flowers and the words sun or shade.
+
+import java.io.*;  // Import class for file input.
+
+public class Flowers {
+   public static void main(String args[]) throws Exception {
+
+      BufferedReader buffFlowers = new BufferedReader(new FileReader("flowers.dat"));
+      String flowerName, sunOrShade;
+      while ((flowerName = buffFlowers.readLine())!=null) {
+          sunOrShade = buffFlowers.readLine();
+          System.out.println(flowerName + " is grown in the " + sunOrShade);
+      }
+      buffFlowers.close();    
+      System.exit(0);
+   } // End of main() method.
+} // End of Flowers class. 
