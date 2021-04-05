a1aa960ba7501fee195d4b9e2aaa034264010777
absolut-ly <44728387+absolut-ly@users.noreply.github.com>
2021-04-05 12:21:26 -0400

    solutions
    Unit10 solutions update. Its not the best but it gets a 100% so meh

--- Unit10/src/CreatingAProgrammerDefinedClass/MyRectangleClassProgram.java
+++ Unit10/src/CreatingAProgrammerDefinedClass/MyRectangleClassProgram.java
@@ -7,22 +7,25 @@ public class MyRectangleClassProgram
     public static void main(String args[])
     {
         // Create rectangle1 and rectangle2 objects here.
-
+        Rectangle rectangle1 = new Rectangle();
+        Rectangle rectangle2 = new Rectangle();
         // Set the length of rectangle1 to 10.0 here.
-
+        rectangle1.setLength(10.0);
         // Set the width of rectangle1 to 5.0 here.
-
+        rectangle1.setWidth(5.0);
 
         // Print the area and perimeter of rectangle1 here.
-
+        System.out.println("Rectangle 1's perimeter is: " + rectangle1.calculatePerimeter(rectangle1.getLength(), rectangle1.getWidth()));
+        System.out.println("Rectangle 1's area is: " + rectangle1.calculateArea(rectangle1.getLength(), rectangle1.getWidth()));
 
         // Set the length of rectangle2 to 7.0 here.
-
+        rectangle2.setLength(7.0);
         // Set the width of rectangle2 to 3.0 here.
-
+        rectangle2.setWidth(3.0);
 
         // Print the area and perimeter of rectangle2 here.
-
+        System.out.println("Rectangle 2's perimeter is: " + rectangle2.calculatePerimeter(rectangle2.getLength(), rectangle2.getWidth()));
+        System.out.println("Rectangle 2's area is: " + rectangle2.calculateArea(rectangle2.getLength(), rectangle2.getWidth()));
 
         System.exit(0);
     }
--- Unit10/src/CreatingAProgrammerDefinedClass/Rectangle.java
+++ Unit10/src/CreatingAProgrammerDefinedClass/Rectangle.java
@@ -3,18 +3,41 @@ package CreatingAProgrammerDefinedClass;
 class Rectangle
 {
     // Length of this rectangle.
+    private Double length;
     // Width of this rectangle.
-
+    private Double width;
 
     // Write set methods here.
 
+    public void setLength(Double length) {
+        this.length = length;
+    }
+
+    public void setWidth(Double width) {
+        this.width = width;
+    }
 
 
     // Write get methods here.
 
+    public Double getLength() {
+        return length;
+    }
+
+    public Double getWidth() {
+        return width;
+    }
 
 
     // Write the calculatePerimeter() and
     // calculateArea() methods here.
+    public Double calculatePerimeter(Double len, Double wid) {
+        Double perimeter = 2*len + 2*wid;
+        return perimeter;
+    }
 
+    public Double calculateArea(Double len, Double wid) {
+        Double area = len * wid;
+        return area;
+    }
 }
